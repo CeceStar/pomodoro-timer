45086805b907bf4847ce595bf6dcfc134097c82d
Cecilia Hallerby <cecilia.hallerby@hyperisland.se>
2021-04-13 10:52:54 +0200

    First commit

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+      </header>
+      <section class="pomodoro-timer-window">
+      <h1>Time to study!</h1>
+      </section>
+
+      <footer className="App-footer">
+
+      </footer>
+    </div>
+  );
+}
+
+export default App;
