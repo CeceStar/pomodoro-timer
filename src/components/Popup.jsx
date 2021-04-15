c7f9b1f6f5a1c4f05e8e7d0145be7e99543107fa
Cecilia Hallerby <cecilia.hallerby@hyperisland.se>
2021-04-15 18:25:55 +0200

    Some indentation changes

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -1,22 +1,23 @@
 import React from "react";
 
-function Popup({isBreakTime, setIsBreakTime}) {
-    return (
-        <div
-          class={isBreakTime ? "dialog" : "dialog hidden"}
-          role="dialog"
-          aria-labelledby="dialog-title"
-          aria-describedby="dialog-description">
-          <h2 id="dialog-title">Time is up!</h2>
-          <p id="dialog-description">Time to take a 5 minutes break.</p>
-          <button
-            type="button"
-            aria-label="Close dialog"
-            class="close-dialog"
-            onClick={() => setIsBreakTime(false)}>
-            <i class="fa fa-times"></i>
-          </button>
-        </div>
-    )};
+function Popup({ isBreakTime, setIsBreakTime }) {
+  return (
+    <div
+      class={isBreakTime ? "dialog" : "dialog hidden"}
+      role="dialog"
+      aria-labelledby="dialog-title"
+      aria-describedby="dialog-description">
+      <h2 id="dialog-title">Time is up!</h2>
+      <p id="dialog-description">Time to take a 5 minutes break.</p>
+      <button
+        type="button"
+        aria-label="Close dialog"
+        class="close-dialog"
+        onClick={() => setIsBreakTime(false)}>
+        <i class="fa fa-times"></i>
+      </button>
+    </div>
+  );
+}
 
-    export default Popup;
+export default Popup;
